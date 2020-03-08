136367727b6a2deaeb19d277c0141ea8c3296a60
Soph <35721420+sophoah@users.noreply.github.com>
2020-03-08 11:01:36 +0800

    adding shardnb as variable

--- staking/multi-bls.sh
+++ staking/multi-bls.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
 
-hmybin="hmy"
+hmybin="./hmy"
 currentdir=$(dirname "$0")
 count=1
 max=80 
 mkdir -p ${currentdir}/.hmy/blskeys
+shardnb=1
 while [ $count -le $max ]
 do
     echo $count
     pubkey=$($hmybin keys generate-bls-key | grep public-key | cut -d '"' -f 4)
     shardgen=$($hmybin utility shard-for-bls $pubkey -n http://34.228.217.167:9500 | jq '.["shard-id"]')
-    if [ $shardgen -eq 1 ]
+    if [ $shardgen -eq $shardnb ]
     then
         cp ${currentdir}/${pubkey}.key ${currentdir}/.hmy/blskeys
         count=$(( $count + 1 ))
